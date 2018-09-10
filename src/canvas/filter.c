4369516403bd856050e67cd60aa261040a7fbbc8
Livenga <ljmmz0405@gmail.com>
2018-09-10 09:27:16 +0900

    sobel フィルタの追加

--- src/canvas/filter.c
+++ src/canvas/filter.c
@@ -6,6 +6,9 @@
 
 #include "canvas/canvas.h"
 
+#define _FLT_TRUE  ((uint8_t)1)
+#define _FLT_FALSE ((uint8_t)0)
+
 
 filter_t *
 filter_create_gaussian(uint32_t width, uint32_t height, double sigma) {
@@ -18,7 +21,7 @@ filter_create_gaussian(uint32_t width, uint32_t height, double sigma) {
 
   flt_ptr->width  = width;
   flt_ptr->height = height;
-  flt_ptr->sigma  = sigma;
+  /*flt_ptr->sigma  = sigma;*/
   flt_ptr->data   = (double *)calloc(width * height, sizeof(double));
 
   if(flt_ptr->data == NULL) {
@@ -40,6 +43,61 @@ filter_create_gaussian(uint32_t width, uint32_t height, double sigma) {
   }
 
   return flt_ptr;
+}
+
+static filter_t *
+_filter_create_sobel(uint8_t is_x) {
+  filter_t *fx_sobel;
+
+  if((fx_sobel = (filter_t *)calloc(1, sizeof(filter_t))) != NULL) {
+    fx_sobel->width  = 3;
+    fx_sobel->height = 3;
+
+    if((fx_sobel->data = (double *)calloc(9, sizeof(double))) != NULL) {
+      if(is_x == _FLT_TRUE) {
+        *(fx_sobel->data + 0) = -1.0;
+        *(fx_sobel->data + 1) =  0.0;
+        *(fx_sobel->data + 2) =  1.0;
+
+        *(fx_sobel->data + 3) = -1.0;
+        *(fx_sobel->data + 4) =  0.0;
+        *(fx_sobel->data + 5) =  1.0;
+        
+        *(fx_sobel->data + 6) = -1.0;
+        *(fx_sobel->data + 7) =  0.0;
+        *(fx_sobel->data + 8) =  1.0;
+      } else {
+        *(fx_sobel->data + 0) = -1.0;
+        *(fx_sobel->data + 1) = -1.0;
+        *(fx_sobel->data + 2) = -1.0;
+
+        *(fx_sobel->data + 3) =  0.0;
+        *(fx_sobel->data + 4) =  0.0;
+        *(fx_sobel->data + 5) =  0.0;
+        
+        *(fx_sobel->data + 6) =  1.0;
+        *(fx_sobel->data + 7) =  1.0;
+        *(fx_sobel->data + 8) =  1.0;
+      }
+    } else {
+      memset((void *)fx_sobel, '\0', sizeof(filter_t));
+      free((void *)fx_sobel);
+      fx_sobel = NULL;
+    }
+  }
+
+  return fx_sobel;
+}
+
+filter_t *
+filter_create_sobel_x(void) {
+  return _filter_create_sobel(/*is_x = */_FLT_TRUE);
+}
+
+
+filter_t *
+filter_create_sobel_y(void) {
+  return _filter_create_sobel(/*is_x*/_FLT_FALSE);
 }
 
 
